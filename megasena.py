5b71a0a01e6b6c40663a45e6471f7aaabe6b7864
Emanuel Gustavo Vierne <emanuelgustavovierne@gmail.com>
2019-08-01 16:24:20 -0300

    Add files via upload

--- megasena.py
+++ megasena.py
@@ -1,17 +1,55 @@
 import random
 
+def teste():
+    apostaTeste = sorteio()
+    sorteado = apostaTeste
+    resultado = verificaResultado(apostaTeste, sorteado)
+    frequenciaAcerto = {
+        1:0,
+        2:0,
+        3:0,
+        4:0,
+        5:0,
+        6:0
+        }
+    if resultado in frequenciaAcerto:
+        frequenciaAcerto[resultado] += 1
+        
+    #print('Aposta teste: {}'.format(apostaTeste))
+    #print('Sorteado: {}'.format(sorteado))
+    print(frequenciaAcerto)
+
 def main():    
     #print(verificaResultado(aposta(), sorteio()))
     acertos = 0
     tentativas = 1
-    while acertos != 6:
+    frequenciaAcerto = {
+        0:0,
+        1:0,
+        2:0,
+        3:0,
+        4:0,
+        5:0,
+        6:0
+        }    
+    freqSorteado = {}
+    while tentativas <= 100000:
         aposta = apostaVencedora()
         sorteados = sorteio()
-        acertos = verificaResultado(aposta, sorteados)
-        tentativas += 1
-        if acertos == 6:
+        resultado = verificaResultado(aposta, sorteados)
+        if resultado == 6:
             print('Aposta Vencedora: {}\nSoteio: {}\nTentativas: {}'.format(aposta, sorteados, tentativas))
         print('Tentativas: {}'.format(tentativas))
+        if resultado in frequenciaAcerto:
+            frequenciaAcerto[resultado] += 1
+        tentativas += 1
+        for dezena in sorteados:
+            if dezena in freqSorteado:
+                freqSorteado[dezena] += 1
+            else:
+                freqSorteado[dezena] = 1
+    print('Frequencia: {}'.format(frequenciaAcerto))        
+    print('Frequencia dezenas sorteadas: {}'.format(freqSorteado))
         
 def aposta():
     aposta = []
@@ -34,7 +72,7 @@ def apostaVencedora():
         dezena = random.randint(1,60)
         if dezena not in aposta:
             aposta.append(dezena)
-    print(aposta)
+    #print(aposta)
     return aposta
 
 def sorteio():
@@ -43,7 +81,7 @@ def sorteio():
         dezena = random.randint(1,60)
         if dezena not in sorteados:
             sorteados.append(dezena)
-    print(sorteados)
+    #print(sorteados)
     return sorteados
 
 def verificaResultado(aposta, sorteados):
@@ -51,8 +89,10 @@ def verificaResultado(aposta, sorteados):
     for dezena in aposta:
         if dezena in sorteados:
             acertos += 1
-    print(acertos)
+    #print(acertos)
     return acertos
+    
+        
 
 main()
-    
+#teste()    
