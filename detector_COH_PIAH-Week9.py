ce2d1153fcaba23f07277a2ae3dc5f3bdcb03e22
Emanuel Gustavo Vierne <emanuelgustavovierne@gmail.com>
2019-08-06 16:48:37 -0300

    Add files via upload

--- detector_COH_PIAH-Week9.py
+++ detector_COH_PIAH-Week9.py
@@ -92,7 +92,7 @@ def calcula_assinatura(texto):
     
     complexidade_sentenca = total_frases_texto(separa_sentencas(texto)) / len(separa_sentencas(texto))
     tamanho_medio_frase = soma_tamanho_frase(separa_sentencas(texto)) / len(separa_sentencas(texto))
-    return tamanho_medio_palavras, relacao_type_token, razao_hapax, tamanho_medio_sentencas, complexidade_sentenca, tamanho_medio_frase
+    return tamanho_medio_palavras
     
 def avalia_textos(textos, ass_cp):
     '''IMPLEMENTAR. Essa funcao recebe uma lista de textos e deve devolver o numero (1 a n) do 
@@ -105,7 +105,7 @@ def n_total_palavras_texto(listaPalavras):
     
 def lista_palavras(texto):
     '''Recebe um texto e retorna uma lista com as palavras do texto'''
-    listaPalavras = re.split(r'[.!?,]+', texto)
+    listaPalavras = texto.split()
     return listaPalavras
 
 def soma_tamanho_palavras(lista_palavras):
@@ -140,10 +140,11 @@ def soma_tamanho_frase(lista_frases):
 def teste():
     #texto = "Muito além, nos confins inexplorados da região mais brega da Borda Ocidental desta Galáxia, há um pequeno sol amarelo e esquecido. Girando em torno deste sol, a uma distancia de cerca de 148 milhões de quilômetros, há um planetinha verde-azulado absolutamente insignificante, cujas formas de vida, descendentes de primatas, são tão extraordinariamente primitivas que ainda acham que relógios digitais são uma grande ideia."
     texto = "Esse texto é para testar, a assinatura do identificador de cohpiah."
-    print(lista_palavras(texto))
+    #print(lista_palavras(texto))
     #print('Total de palavras no texto: {}'.format(total_palavras_texto(lista_palavras(texto))))
     print(calcula_assinatura(texto))
-    print(separa_sentencas(texto))
+    print(lista_palavras(texto))
+    #print(separa_sentencas(texto))
 
 def main():
     '''Gerencia o detector de coh-piah'''
