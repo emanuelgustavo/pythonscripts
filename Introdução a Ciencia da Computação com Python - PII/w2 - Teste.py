47116ce23e593da3a0288c35350fef5037f651c7
Emanuel Gustavo Vierne <emanuelgustavovierne@gmail.com>
2019-08-16 12:53:38 -0300

    Add files via upload

--- Introdução a Ciencia da Computação com Python - PII/w2 - Teste.py
+++ Introdução a Ciencia da Computação com Python - PII/w2 - Teste.py
@@ -1,9 +1,11 @@
 def fazAlgo(string):
     pos = 0
+    #string = string.lower()
     string1 = ""
     while pos < len(string):
         if string[pos] != " ":
-            string1 = string1 + string[pos].lower()
+            string1 = string1 + string[pos]
+        string1 = string1.capitalize()
         pos = pos + 1
     return string1
 
