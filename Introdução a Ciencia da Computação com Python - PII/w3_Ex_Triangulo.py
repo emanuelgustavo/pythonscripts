34f7216436ea0d9b871e77394ac5379fbeb35f4d
Emanuel Gustavo Vierne <emanuelgustavovierne@gmail.com>
2019-08-26 07:15:09 -0300

    20/20

--- Introdução a Ciencia da Computação com Python - PII/w3_Ex_Triangulo.py
+++ Introdução a Ciencia da Computação com Python - PII/w3_Ex_Triangulo.py
@@ -2,18 +2,17 @@
 Exercício 1: Uma classe para triângulos
 Defina a classe Triangulo cujo construtor recebe 3 valores inteiros correspondentes aos 
 lados a, b e c de um triângulo.
-
 A classe triângulo também deve possuir um método perimetro, que não recebe parâmetros 
 e devolve um valor inteiro correspondente ao perímetro do triângulo.
 '''
 
 class Triangulo:
     
-    def __init__(self, lado_a, lado_b, lado_c):
-        self.lado_a = lado_a
-        self.lado_b = lado_b
-        self.lado_c = lado_c
-        
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+    '''   
     def a(self):
         return self.lado_a
     
@@ -22,14 +21,14 @@ class Triangulo:
     
     def c(self):
         return self.lado_c
-    
+    '''
     def perimetro(self):
-        return self.lado_a + self.lado_b + self.lado_c
+        return self.a + self.b + self.c
     
-    def tipo_lado(self, a, b, c):
-        if a + b + c == 3 * a:
+    def tipo_lado(self):
+        if self.a + self.b + self.c == 3 * self.a:
             return 'equilátero'
-        elif self.lado_a != self.lado_b != self.lado_c:
+        elif self.a != self.b and self.a != self.c and self.b != self.c:
             return 'escaleno'
         else:
             return 'isóceles'
