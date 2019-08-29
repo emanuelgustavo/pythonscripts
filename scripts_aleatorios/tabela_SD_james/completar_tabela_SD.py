24b1597a7f9a3020e8c586a1dc7921ad7373532a
Emanuel Gustavo Vierne <emanuelgustavovierne@gmail.com>
2019-08-29 07:38:30 -0300

    refatoring...

--- scripts_aleatorios/tabela_SD_james/completar_tabela_SD.py
+++ scripts_aleatorios/tabela_SD_james/completar_tabela_SD.py
@@ -42,10 +42,29 @@ for produto in lista_produtos:
     linha = ';'
     linha = linha.join(produto.produtos_atributos())    
     print(linha)
-    print(produto.produtos_atributos())
     arq_lista_produtos.write(linha)
 
 arq_lista_produtos.close()
+
+def escreve_string(produto, contador_combinacao_produto, acab, estofaria=False, faixa_tecido=''):
+
+    if estofaria:
+        string_estofaria = ' DE TECIDO SEM COMPOSE '
+    else:
+        string_estofaria = ''
+
+    string_linha = ''
+    string_pt1 = ''
+    string_pt2 = ''
+    string_pt3 = ''
+    string_pt1 += (cnpj + '|' + produto.cod_produto + ' ' + acab + '-' + str(contador_combinacao_produto) + ('\t'*3))
+    string_pt2 += ('|' + produto.nome_produto + ' ' + acab + ' ' + faixa_tecido + string_estofaria + produto.caracteristica_produto + \
+                '(' + produto.tamanho_produto + ')')
+    string_pt3 += ('|UN |' + produto.ncm_produto + '|NAO|00000|00000|00000|00000' + produto.valor_produto + '|000000000000|000000000|000000000|')
+    string_linha += string_pt1 + string_pt2 + ( ('\t'*4) if len(string_pt2) >= 64 else ('\t'*5) ) + string_pt3 + '\n'
+    print('escreve_string:\t', string_linha)
+    return string_linha
+    
 
 for produto in lista_produtos:
 
@@ -55,88 +74,29 @@ for produto in lista_produtos:
         if tem_estofaria:
             for faixa in faixa_tecidos:
                 acab = acabamento_nogueira
-                string_estofaria = ' DE TECIDO SEM COMPOSE '
-                string_linha = ''
-                string_pt1 = ''
-                string_pt2 = ''
-                string_pt3 = ''
-                string_pt1 += (cnpj + '|' + produto.cod_produto + ' ' + acab + '-' + str(contador_combinacao_produto) + ('\t'*3))
-                string_pt2 += ('|' + produto.nome_produto + ' ' + acab + ' ' + (faixa + string_estofaria if tem_estofaria else produto.caracteristica_produto) + \
-                            '(' + produto.tamanho_produto + ')')
-                string_pt3 += ('|UN |' + produto.ncm_produto + '|NAO|00000|00000|00000|00000' + produto.valor_produto + '|000000000000|000000000|000000000|')
-                string_linha += string_pt1 + string_pt2 + ( ('\t'*4) if len(string_pt2) >= 64 else ('\t'*5) ) + string_pt3 + '\n'
-
-                tabela_produtos_novos += string_linha
+                tabela_produtos_novos += escreve_string(produto, contador_combinacao_produto, acab, True, faixa)
                 contador_combinacao_produto += 1
         else:
             acab = acabamento_nogueira
-            string_estofaria = ' DE TECIDO SEM COMPOSE '
-            string_linha = ''
-            string_pt1 = ''
-            string_pt2 = ''
-            string_pt3 = ''
-            string_pt1 += (cnpj + '|' + produto.cod_produto + ' ' + acab + '-' + str(contador_combinacao_produto) + ('\t'*3))
-            string_pt2 += ('|' + produto.nome_produto + ' ' + acab + ' ' + (faixa + string_estofaria if tem_estofaria else produto.caracteristica_produto) + \
-                        '(' + produto.tamanho_produto + ')')
-            string_pt3 += ('|UN |' + produto.ncm_produto + '|NAO|00000|00000|00000|00000' + produto.valor_produto + \
-                        '|000000000000|000000000|000000000|')
-            string_linha += string_pt1 + string_pt2 + ( ('\t'*4) if len(string_pt2) >= 64 else ('\t'*5) ) + string_pt3 + '\n'
-
-            tabela_produtos_novos += string_linha
+            tabela_produtos_novos += escreve_string(produto, contador_combinacao_produto, acab, True)
             contador_combinacao_produto += 1
 
     else:
         if tem_estofaria:
             for faixa in faixa_tecidos:
                 for acab in acabamentos:
-                    string_linha = ''
-                    string_pt1 = ''
-                    string_pt2 = ''
-                    string_pt3 = ''
-                    string_pt1 += (cnpj + '|' + produto.cod_produto + ' ' + acab + '-' + str(contador_combinacao_produto) + ('\t'*3))
-                    string_pt2 += ('|' + produto.nome_produto + ' ' + acab + ' ' + (produto.caracteristica_produto) + \
-                                     '(' + produto.tamanho_produto + ')')
-                    string_pt3 += ('|UN |' + produto.ncm_produto + '|NAO|00000|00000|00000|00000' + produto.valor_produto + \
-                                      '|000000000000|000000000|000000000|')
-                    string_linha += string_pt1 + string_pt2 + ( ('\t'*4) if len(string_pt2) >= 64 else ('\t'*5) ) + string_pt3 + '\n'
-
-                    tabela_produtos_novos += string_linha
+                    tabela_produtos_novos += escreve_string(produto, contador_combinacao_produto, acab, True, faixa)
                     contador_combinacao_produto += 1
+                    
         else:
-            acab = acabamento_nogueira
-            string_estofaria = ' DE TECIDO SEM COMPOSE '
-            string_linha = ''
-            string_pt1 = ''
-            string_pt2 = ''
-            string_pt3 = ''
-            string_pt1 += (cnpj + '|' + produto.cod_produto + ' ' + acab + '-' + str(contador_combinacao_produto) + ('\t'*3))
-            string_pt2 += ('|' + produto.nome_produto + ' ' + acab + ' ' + (faixa + string_estofaria if tem_estofaria else produto.caracteristica_produto) + \
-                        '(' + produto.tamanho_produto + ')')
-            string_pt3 += ('|UN |' + produto.ncm_produto + '|NAO|00000|00000|00000|00000' + produto.valor_produto + \
-                        '|000000000000|000000000|000000000|')
-            string_linha += string_pt1 + string_pt2 + ( ('\t'*4) if len(string_pt2) >= 64 else ('\t'*5) ) + string_pt3 + '\n'
-
-            tabela_produtos_novos += string_linha
-            contador_combinacao_produto += 1
+            for acab in acabamentos:
+                tabela_produtos_novos += escreve_string(produto, contador_combinacao_produto, acab)
+                contador_combinacao_produto += 1
 
     if not tem_estofaria:
         for acab in acabamentos:
-            string_linha = ''
-            string_pt1 = ''
-            string_pt2 = ''
-            string_pt3 = ''
-            string_pt1 += (cnpj + '|' + produto.cod_produto + ' ' + acab + '-' + str(contador_combinacao_produto) + ('\t'*3))
-            string_pt2 += ('|' + produto.nome_produto + ' ' + acab + ' ' + (produto.caracteristica_produto) + \
-                             '(' + produto.tamanho_produto + ')')
-            string_pt3 += ('|UN |' + produto.ncm_produto + '|NAO|00000|00000|00000|00000' + produto.valor_produto + \
-                              '|000000000000|000000000|000000000|')
-            string_linha += string_pt1 + string_pt2 + ( ('\t'*4) if len(string_pt2) >= 64 else ('\t'*5) ) + string_pt3 + '\n'
-
-            tabela_produtos_novos += string_linha
+            tabela_produtos_novos += escreve_string(produto, contador_combinacao_produto, acab)
             contador_combinacao_produto += 1
-
-
-    #opcao = input('Cadastrar mais produtos? s/n: ')
 
 arq_tabela.write(tabela_produtos_novos)
 arq_tabela.close()
