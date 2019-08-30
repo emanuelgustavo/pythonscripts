dbaf0c364a8eec65b388a656e333d1ed06406997
Emanuel Gustavo Vierne <emanuelgustavovierne@gmail.com>
2019-08-30 14:21:18 -0300

    Add files via upload

--- Introdução a Ciencia da Computação com Python - PII/w4_busca_sequencial/w4_busca_seq_telefones.py
+++ Introdução a Ciencia da Computação com Python - PII/w4_busca_sequencial/w4_busca_seq_telefones.py
@@ -4,6 +4,21 @@ Lista com 2 milhões de telefones aleatórios
 '''
 import random
 
+def busca_contato(nome):
+    arq_lista = open('lista_telefones.txt', 'r')
+    lista_telefones = arq_lista.readlines()
+    lista_contatos = {}
+    for linha in lista_telefones:
+        contato = []
+        contato = linha.split()
+        lista_contatos[contato[0]] = contato[1]
+
+    for contato in lista_contatos:
+        if contato == nome:
+            return [ contato, lista_contatos[contato] ]
+        else:
+            return print('nenhum contato com esse nome')
+
 def gera_telefone():
     
     numero_telefone= ''
@@ -24,7 +39,7 @@ nomes = arq_nomes.readlines()
 for index in range(len(nomes)):
     nomes[index] = nomes[index].rstrip('\n')
 
-for i in range(2000000):
+for i in range(100):
     nome = nomes[random.randint(0, len(nomes)-1)]
     telefone = gera_telefone()
     lista_telefonica[nome] = telefone
@@ -34,7 +49,7 @@ arq_lista_telefone = open('lista_telefones.txt', 'w')
 
 for contato in lista_telefonica:
     str_contato = ''
-    str_contato += contato + ' => ' + lista_telefonica[contato] + '\n'
+    str_contato += contato + ' ' + lista_telefonica[contato] + '\n'
     arq_lista_telefone.write(str_contato)
 
 arq_lista_telefone.close()
