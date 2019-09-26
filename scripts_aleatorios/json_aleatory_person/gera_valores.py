4f444b580860a849d01afb6f64a75dc39a06b040
Emanuel Gustavo Vierne <emanuelgustavovierne@gmail.com>
2019-09-26 13:27:08 -0300

    gera telefone

--- scripts_aleatorios/json_aleatory_person/gera_valores.py
+++ scripts_aleatorios/json_aleatory_person/gera_valores.py
@@ -10,7 +10,7 @@ class GeraValores:
         for nome in lista_nomes:
             novo_nome = re.sub('\n', '', nome)
             nova_lista_nomes.append(novo_nome)
-        print(nova_lista_nomes)
+        #print(nova_lista_nomes)
         return nova_lista_nomes
     
     def carrega_nome(self):
@@ -31,17 +31,27 @@ class GeraValores:
         nome = sobrenomes[random.randint(0, len(sobrenomes)-1)]
         return nome
 
+    #carrega os nomes do arquivo txt
     def carrega_arquivo(self, arquivo):
         with open(arquivo, 'r') as arq_lista_nomes:
             lista_arquivo = arq_lista_nomes.readlines()
         lista_arquivo = self.limpa_string_nome(lista_arquivo)
         return lista_arquivo
-    #carrega os nomes do arquivo txt
+    
+    def numero_telefone(self):
+        telefone = ''
+        for i in range(9):
+            if i == 4:
+                telefone += '-'
+            else:
+                telefone += str(random.randint(0,9))
+        return telefone
 
 def main():
     gv = GeraValores()
     nome_completo = gv.gera_nome() + ' ' + gv.gera_sobrenome()
-    print(nome_completo)
+    telefone = gv.numero_telefone()
+    print(nome_completo, telefone)
 
 if __name__ == "__main__":
     main()
