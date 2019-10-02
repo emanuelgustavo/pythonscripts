406a45f5277383c6fba9fcc594e8fdfcdd41d03e
Emanuel Gustavo Vierne <emanuelgustavovierne@gmail.com>
2019-10-02 16:53:59 -0300

    Add files via upload

--- Introdução a Ciencia da Computação com Python - PII/w5_busca_binária/w5_Ex2_bubble_sort.py
+++ Introdução a Ciencia da Computação com Python - PII/w5_busca_binária/w5_Ex2_bubble_sort.py
@@ -1,23 +1,21 @@
 def bubble_sort(lista):
-    
-    for x in range(len(lista)-1):        
-        y = x + 1
-        while y < len(lista):            
-            a = lista[x]
-            b = lista[y]       
-            if a > b:
+    houve_troca = True
+    print(lista)
+    while houve_troca:        
+        houve_troca = False        
+        for x in range(1, len(lista)):
+            y = x - 1
+            if lista[x] < lista[y]:
                 lista[x], lista[y] = lista[y], lista[x]
-                x += 1            
-                print(lista) 
-            y += 1
-    
+                houve_troca = True
+            if houve_troca:    
+                print(lista)
     return lista
-    
-    
-    
+'''    
 def test():
     #print(bubble_sort([5, 1, 4, 2]))
     #bubble_sort([5, 1, 4, 2])
-    bubble_sort([1, 5, 3, 4, 2, 0])
+    print(bubble_sort([1, 5, 3, 4, 2, 0]))
     
 test()
+'''
