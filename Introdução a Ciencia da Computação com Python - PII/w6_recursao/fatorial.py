1a68964b3b5e301f4e7f06bbe02e9839bde8e7f0
Emanuel Gustavo Vierne <emanuelgustavovierne@gmail.com>
2019-10-10 13:02:03 -0300

    Add files via upload

---
+++ Introdução a Ciencia da Computação com Python - PII/w6_recursao/fatorial.py
@@ -0,0 +1,19 @@
+import pytest
+
+def fatorial(n):
+    if n < 1:
+        return 1
+    else:
+        return n * fatorial(n-1)
+
+@pytest.mark.parametrize('entrada, esperado', [
+    (0, 1),
+    (1, 1),
+    (2, 2),
+    (3, 6),
+    (4, 24),
+    (5, 120)
+    ])
+
+def test_fatorial(entrada, esperado):
+    assert fatorial(entrada) == esperado
