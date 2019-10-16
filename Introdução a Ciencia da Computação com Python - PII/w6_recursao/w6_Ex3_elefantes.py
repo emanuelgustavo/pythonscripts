553385b9ea3af52ad03e3f567f5ed331aaf14514
Emanuel Gustavo Vierne <emanuelgustavovierne@gmail.com>
2019-10-16 17:25:45 -0300

    Add files via upload

--- Introdução a Ciencia da Computação com Python - PII/w6_recursao/w6_Ex3_elefantes.py
+++ Introdução a Ciencia da Computação com Python - PII/w6_recursao/w6_Ex3_elefantes.py
@@ -6,4 +6,15 @@ def incomodam(n):
             return 'incomoda '    
         return 'incomodam ' + incomodam(n-1)
     
-print(incomodam(5))
+def elefantes(n, string=''):
+    if n < 0:
+        return ''
+    else:
+        if n == 1:
+            string += ('Um elefante ' + incomodam(n) + 'muita gente\n') 
+            return string
+        else:
+            string += ('{} elefantes '.format(n) + incomodam(n) + 'muito mais\n')
+            return string + elefantes(n-1)
+        
+print(elefantes(5))
