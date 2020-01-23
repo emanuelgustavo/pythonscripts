1e54b6b495583e65ae56f8be5357ed6edd03e544
Emanuel Gustavo Vierne <emanuelgustavovierne@gmail.com>
2020-01-22 21:50:06 -0300

    alterando sorteio

--- loteria.py
+++ loteria.py
@@ -1,14 +1,19 @@
 import random
 
 def sorteio():
-    
+    pass
+
 def aposta():
     aposta = []
-    numero = int(input('Digite um numero de 1 a 60'))
-    while numero < 1 or numero > 60:
-        pass
-    else:
-        pass
+    while len(aposta) < 6:
+        numero = int(input('Digite um numero de 1 a 60'))
+        while numero < 1 or numero > 60:
+             numero = int(input('Digite novamente!'))
+        else:
+            aposta.append(numero)
+    return aposta
+
 def main():
+    print(aposta())
     
 main()
